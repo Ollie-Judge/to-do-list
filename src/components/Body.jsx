b2c3144ef60254ad82af615a72fe2ac2ce69acf5
Ollie-Judge <ollie.judge@hotmail.com>
2022-06-07 15:41:43 +0100

    init commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="body">
+      <h1>To Do</h1>
+      <p>This is the note content</p>
+    </div>
+  );
+}
+
+export default Body;
